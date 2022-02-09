ce6681f7d2520a48dd0d5008430ef9dc4b5ab4e4
Canh.Nguyen <leagueyasuo186@gmail.com>
2022-02-09 16:55:27 +0700

    xoaphantu

--- src/xoaphantu.java
+++ src/xoaphantu.java
@@ -2,26 +2,25 @@ import java.util.Scanner;
 
 public class xoaphantu {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-        int[] newArr = arr;
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] newArr;
         int index;
-        int so;
+        int c = 0;
         Scanner scanner = new Scanner(System.in);
-        do {
-            System.out.println("nhap thu tu phan tu muon xoa");
-            index = scanner.nextInt();
-            if (index > 10)
-                System.out.println("khong nhap qua so 10");
-        } while (index > 10);
-        System.out.println("ban muon doi thanh so may? ");
-        so = scanner.nextInt();
-        arr[index - 1] = so;
+        System.out.println("nhap phan tu muon xoa: ");
+        index = scanner.nextInt();
+        newArr = new int[arr.length - 1];
         for (int i = 0; i < arr.length; i++) {
-            if (i = (index-1)){
-                arr[i] = so;
-            } System.out.println("moi moi se la: " + arr[i] + "\t");
-
+            if(arr[i] != index){
+                arr[c] = arr[i];
+                c++;
+            }
+        }
+        System.out.println("mang moi la: ");
+        for (int i = 0; i < c; i++) {
+            System.out.print(arr[i] + "\t");
         }
     }
-}
+    }
+
 
